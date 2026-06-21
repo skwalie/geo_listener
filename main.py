dff55a9bfd8438176958beac3eec74acddf94e9f
skwalie <pascal.maquinay@gmail.com>
2026-06-21 13:03:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,46 @@
-import json
-from datetime import datetime
 
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from sqlmodel import Session, create_engine, select
 
-from repository import Trace, TraceCollection
+from repository import Trace, create_db_and_tables, Traces
 
 app = FastAPI()
 
+origins = [
+    "*"
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+   # allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+engine = create_engine(url="postgresql://neondb_owner:npg_YH24WkdKUDul@ep-wandering-dream-alrifq4w.c-3.eu-central-1.aws.neon.tech/neondb?sslmode=require")
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables(engine)
 
 @app.get("/")
 async def root():
-    pass
+    with Session(engine) as session:
+        traces = session.exec(select(Traces)).all()
+
+        return list(sorted(traces, key=lambda trace: trace.timestamp, reverse=True))
 
 @app.post("/trace/")
 async def trace_position(position: Trace, request: Request):
-    collection = TraceCollection(traces=[])
-
     try:
-        with open(f"./out/positions-{datetime.today().strftime('%Y-%m-%d')}.json", "r", encoding="utf-8") as f:
-            content = json.load(f)
-            collection = TraceCollection(**content)
+        traces = Traces(timestamp=position.timestamp, host=position.host, long=position.long, lat=position.lat)
+        with Session(engine) as session:
+            traces.ip = request.client.host.format()
+
+            session.add(traces)
+            session.commit()
 
     except Exception as e:
         print("can't read positions from file")
-
-    position.ip = request.client.host.format()
-    collection.traces.append(position)
-
-    try:
-        with open(f"./out/positions-{datetime.today().strftime('%Y-%m-%d')}.json", "w", encoding="utf-8") as f:
-            f.write(collection.model_dump_json())
-    except Exception as e:
-        print("can't write positions to file")
-
-
