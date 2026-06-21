c829bfbb9979292a519fe0634d4aa102b3cc862c
skwalie <pascal.maquinay@gmail.com>
2026-06-21 13:03:42 +0200

    Update repository.py

--- repository.py
+++ repository.py
@@ -1,12 +1,25 @@
 from pydantic import BaseModel
 
 
+from sqlmodel import Field, SQLModel
+
+
 class Trace(BaseModel):
     timestamp: int
     host: str
     long: float
     lat: float
-    ip: str
+    ip: str | None = None
 
-class TraceCollection(BaseModel):
-    traces: list[Trace]
+
+class Traces(SQLModel, table=True):
+    timestamp: int | None = Field(default=None, primary_key=True)
+    host: str
+    long: float
+    lat: float
+    ip: str | None = None
+
+
+def create_db_and_tables(engine):
+    SQLModel.metadata.create_all(engine)
+
